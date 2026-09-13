0c48018d592cb922bccb1ba06f7949a3f8dcbaa9
Abdul Wahab Mughal <100358513+wahab-prog@users.noreply.github.com>
2026-09-13 17:58:57 +0500

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -27,6 +27,25 @@ module.exports = defineConfig({
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   modules: [
-    { key: "api_key", resolve: "@medusajs/medusa/api-key" },
-  ],
+  { key: "api_key", resolve: "@medusajs/medusa/api-key" },
+  {
+    resolve: "@medusajs/medusa/file",
+    options: {
+      providers: [
+        {
+          resolve: "@medusajs/medusa/file-s3",
+          id: "s3",
+          options: {
+            file_url: process.env.S3_FILE_URL,
+            access_key_id: process.env.S3_ACCESS_KEY_ID,
+            secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+            region: process.env.S3_REGION,
+            bucket: process.env.S3_BUCKET,
+            endpoint: process.env.S3_ENDPOINT,
+          },
+        },
+      ],
+    },
+  },
+],
 })
